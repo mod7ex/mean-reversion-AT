45cdeb8734cbc684fd6ded3b45466f7ed0610e86
Mourad EL CADI <mouradface1998@gmail.com>
2024-09-21 19:36:51 +0100

    save

--- helpers.py
+++ helpers.py
@@ -1,72 +1,69 @@
 class Trade:
     def __init__(self, entry = 0.0, tp = 0.0, sl = 0.0, on = False):
         # Private variables
-        self._on = on
-        self._entry = entry
-        self._tp = tp
-        self._sl = sl
+        self.__on = on
+        self.__entry = entry
+        self.__tp = tp
+        self.__sl = sl
 
     @property
     def entry(self):
         """Getter for entry."""
-        return self._entry
+        return self.__entry
 
     @property
     def tp(self):
         """Getter for tp."""
-        return self._tp
+        return self.__tp
 
     @property
     def sl(self):
         """Getter for sl."""
-        return self._sl
+        return self.__sl
     
     @property
     def on(self):
         """Getter for on."""
-        return self._on
+        return self.__on
     
     # Computed properties
     @property
     def is_buy(self):
-        return self._tp > self._sl
+        return self.__tp > self.__sl
     
     @property
     def is_sell(self):
-        return self._tp < self._sl
+        return self.__tp < self.__sl
 
     @entry.setter
     def entry(self, value):
         if isinstance(value, float) or isinstance(value, int) :
-            if self._on:
-                self._entry = value
+            if self.__on:
+                self.__entry = value
         else:
             raise ValueError("Entry must be a floating number")
 
     @tp.setter
     def tp(self, value):
         if isinstance(value, float) or isinstance(value, int) :
-            if self._on:
-                self._tp = value
+            if self.__on:
+                self.__tp = value
         else:
             raise ValueError("TP must be a floating number")
 
     @sl.setter
     def sl(self, value):
         if isinstance(value, float) or isinstance(value, int) :
-            if self._on:
-                self._sl = value
+            if self.__on:
+                self.__sl = value
         else:
             raise ValueError("SL must be a floating number")
         
-    @on.setter
-    def on(self, value):
-        if isinstance(value, bool):
-            if not value: 
-                self._entry = 0
-                self._tp = 0
-                self._sl = 0
-
-            self._on = value
-        else:
-            raise ValueError("On must be a boolean")
+    def On(self):
+        self.__on = True
+    
+    def Off(self):
+        self.__entry = 0
+        self.__tp = 0
+        self.__sl = 0
+        self.__on = False
